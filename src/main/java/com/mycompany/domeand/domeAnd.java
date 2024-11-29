4338e6fead7f7d7a64b48bbb117682a57ec74c81
ANDREAMELANY <andreamontanio2250281298@gmail.com>
2024-11-29 16:32:27 -0500

    hola

--- src/main/java/com/mycompany/domeand/domeAnd.java
+++ src/main/java/com/mycompany/domeand/domeAnd.java
@@ -1,20 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package com.mycompany.domeand;
 
-/**
- *
- * @author and_j
- */
+
 public class domeAnd {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
     
+    public static void main(String[] args) {
+        System.out.println("Hola Domenica");
+    }
+   
+    
+     
 }
